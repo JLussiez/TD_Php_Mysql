1efdcdbc86bcdb8f93cc38083430e40a864ff08d
Julien Lussiez <jlussiez.la-providence.net>
2020-12-03 12:05:24 +0100

    index

---
+++ Index.php
@@ -0,0 +1,53 @@
+<html>
+
+    <head>
+        <link href="style.css" rel="stylesheet">
+        <title>TP Web</title>
+    </head>
+
+    <body>
+
+        <h1>Bienvenue sur l'Index de mes exercices :</h1>
+
+        <div id="header">
+
+            <ul id="menu">
+
+    <li><a href="#">Exercices HTML</a>
+        <ul>
+            <li><a href="../exo html/exo1.html">Exercice n°1</a></li>
+            <li><a href="../exo html/exo2.html">Exercice n°2</a></li>
+            <li><a href="../exo html/exo3.html">Exercice n°3</a></li>
+            <li><a href="../exo html/exo4.html">Exercice n°4</a></li>
+            <li><a href="../exo html/exo5.html">Exercice n°5</a></li>
+        </ul>
+    </li>
+
+    <li><a href="#">Exercices CSS</a>
+        <ul>
+            <li><a href="../Exo CSS/Exo1CSS.html">Exercice n°1</a></li>
+            <li><a href="../Exo CSS/Exo2CSS.html">Exercice n°2</a></li>
+            <li><a href="../Exo CSS/Exo3CSS.html">Exercice n°3</a></li>
+            <li><a href="../Exo CSS/Exo4CSS.html">Exercice n°4</a></li>
+        </ul>
+    </li>
+
+    <li><a href="#">Exercices PHP Partie 1</a>
+        <ul>
+            <li><a href="http://192.168.65.60/julien/TPWEB/PhP/TP1/Ex1/Ex1.php">Exercice n°1</a></li>
+            <li><a href="http://192.168.65.60/julien/TPWEB/PhP/TP1/Ex2/Ex2.1.php">Exercice n°2.1</a></li>
+            <li><a href="http://192.168.65.60/julien/TPWEB/PhP/TP1/Ex2/Ex2.2.php">Exercice n°2.2</a></li>
+            <li><a href="http://192.168.65.60/julien/TPWEB/PhP/TP1/Ex3/Ex3.php">Exercice n°3</a></li>
+            <li><a href="http://192.168.65.60/julien/TPWEB/PhP/TP1/Ex4/Ex4.php">Exercice n°4</a></li>
+            <li><a href="http://192.168.65.60/julien/TPWEB/PhP/TP1/Ex5/Ex5.php">Exercice n°5</a></li>
+            <li><a href="http://192.168.65.60/julien/TPWEB/PhP/TP1/Ex6/Ex6.php">Exercice n°6</a></li>
+            <li><a href="http://192.168.65.60/julien/TPWEB/PhP/TP1/Ex7/Ex7.php">Exercice n°7</a></li>
+            <li><a href="http://192.168.65.60/julien/TPWEB/PhP/TP1/Ex8/Ex8.php">Exercice n°7</a></li>
+        </ul>
+    </li>
+</ul>
+
+</div>
+
+    </body>
+</html>
